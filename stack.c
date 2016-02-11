98d395c92545b98c9a9e8eb428f57bce9096bff7
Mayank Yadav <mayank@hackfestism.org>
2016-02-11 13:17:33 +0530

    Added comments for instructions.

--- stack.c
+++ stack.c
@@ -9,39 +9,63 @@ typedef struct Stack {
     DLL* elements;
 } Stack;
 
-Stack* createStack() {
-    Stack* stack = (Stack*)malloc(sizeof(Stack));
-    stack->elements = createDLL();
+/*
+    In this function, you have to allocate space for stack using malloc function.
+    Then you have to initialize the elements DLL using the function createDLL() defined in DLL.h
 
-    return stack;
+    Then you have to return the newly allocated and initialized stack.
+*/
+
+Stack* createStack() {
+
 }
+
+/*
+    Use the appropriate function from the DLL.h file and perform the push operation on stack.
+*/
 
 void push(Stack* stack,int data) {
-    insertDLLNodeFront(stack->elements,data);
+
 }
+
+/*
+    Use the appropriate function from the DLL.h file and perform the pop operation on stack.
+*/
 
 void pop(Stack* stack) {
-    removeDLLNodeFront(stack->elements);
+
 }
+
+/*
+    Return true if stack is empty, else return false.
+*/
 
 int isEmpty(Stack* stack) {
-    int len = DLLSize(stack->elements);
-    if(len == 0)
-        return true;
-    return false;
+
 }
+
+/*
+    Get the top node of the stack.
+*/
 
 DLLNode* getTop(Stack* stack) {
-    return stack->elements->head;
+
 }
+
+/*
+    Free all the memory occupied by stack.
+
+    Hint: while the stack is not empty, keep on popping.
+    Then free stack.
+*/
 
 void deleteStack(Stack* stack) {
-    while(!isEmpty(stack)) {
-        pop(stack);
-    }
 
-    free(stack);
 }
+
+/*
+    You don't need to change this.
+*/
 
 int main() {
     Stack* stack = createStack();
